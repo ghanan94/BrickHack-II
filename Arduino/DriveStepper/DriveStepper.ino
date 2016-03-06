96e2be422e4537644f04045518041459caf38115
Anthony Tenggoro <anthony.tenggoro@gmail.com>
2016-03-06 09:01:10 -0500

    Updated drivestepper

--- Arduino/DriveStepper/DriveStepper.ino
+++ Arduino/DriveStepper/DriveStepper.ino
@@ -20,20 +20,40 @@ Stepper stepper(STEPS, 8, 9, 10, 11);
 
 // the previous reading from the analog input
 int previous = 0;
+int i = 0;
 
 void setup() {
   // set the speed of the motor to 30 RPMs
   stepper.setSpeed(50);
+  
 }
 
 void loop() {
   // get the sensor value
   int val = analogRead(0);
-
+  Serial.println(val);
   // move a number of steps equal to the change in the
   // sensor reading
-  stepper.step(val - previous);
+  if( i < 10000){
+    stepper.step(val - previous);
+  }
+  else if (i >= 10000 && i < 20000){
+    stepper.step(previous - val);
+  }
 
-  // remember the previous value of the sensor
+  else{
+    i = 0;
+  }
+
   previous = val;
+  i++;
+
+  
+  //stepper.step(0);
+  //delay(2000);
+  //stepper.step(200);
+  //delay(2000);
+  //stepper.step(0);
+  //delay(2000);
+  
 }
